6dbc6b2cf42ac051296a6d15c3a38cfb16cab47e
Jay Dainn <37037667+jaydainn@users.noreply.github.com>
2020-02-17 17:16:35 +0100

    signup not working

--- navbar/signup.php
+++ navbar/signup.php
@@ -4,16 +4,16 @@ $firstname =$_POST['firstname'];
 $lastname =$_POST['lastname'];
 $email = $_POST['email'];
 $password = $_POST['password'];
-$cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','root','root');
-$sql = $cnnx -> prepare( "INSERT INTO `users` (`username`, `email`, `firstname`, `lastname`, `password`, `type`) VALUES (':username', ':email', ':firstname', ':lastname', ':password', ':type')");
-$sql -> bindParam(':username', $username);
-$sql -> bindParam(':email', $email);
-$sql -> bindParam(':firstname', $firstname);
-$sql -> bindParam(':lastname', $lastname);
-$sql -> bindParam(':password', $password);
-$sql -> bindParam(':type', \'user\' );
+$type ="user";
+$cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','admin','root');
+$sql = $cnnx -> prepare( "INSERT INTO `users` (`username`, `email`, `firstname`, `lastname`, `password`, `type`) VALUES (:username, :email, :firstname, :lastname, :password, :type)");
+$sql -> bindParam(':username', $username, PDO::PARAM_STR);
+$sql -> bindParam(':email', $email, PDO::PARAM_STR);
+$sql -> bindParam(':firstname', $firstname, PDO::PARAM_STR);
+$sql -> bindParam(':lastname', $lastname, PDO::PARAM_STR);
+$sql -> bindParam(':password', $password, PDO::PARAM_STR);
+$sql -> bindParam(':type', $user, PDO::PARAM_STR );
 $sql -> execute();
-
 
 
 
