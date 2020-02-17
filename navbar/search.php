58cc1acb3356ed8fa0a5caa7ad7a15e6b06a7100
Jay Dainn <37037667+jaydainn@users.noreply.github.com>
2020-02-17 18:20:26 +0100

    search bar & php

---
+++ navbar/search.php
@@ -0,0 +1,10 @@
+<?php
+$searchtxt = $_GET['search'];
+$cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','admin','root');
+$sql ="SELECT * FROM `movie` WHERE title = '/*$searchtxt*'/ ";);
+foreach($cnnx -> $sql as $row){
+
+}
+
+>
+
