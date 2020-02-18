a8a64af34525232de225625f57d2102dec942642
Phil KREJCI <kphil@protonmail.com>
2020-02-18 17:30:39 +0100

    dscds

--- navbar/login.php
+++ navbar/login.php
@@ -2,13 +2,13 @@
 
 session_start();
 if(isset($_SESSION['type'])){
-    header('location: navbar.php');
+    header('location: ../index.php');
 }
 else{
     $email = $_POST['email'];
     $password = $_POST['password'];
     $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','yellowtree','yellow');
-    $sql = $cnnx -> prepare("SELECT type , username FROM `users` WHERE email = :email AND password = :password ");
+    $sql = $cnnx -> prepare("SELECT type , username FROM `USERS` WHERE email = :email AND password = :password ");
     $sql -> execute([':email' => $email, ':password' => $password]);
     $type = $sql -> fetchAll();
     foreach($type as $types){
@@ -16,7 +16,7 @@ else{
          $_SESSION['username'] = $types['username'] ;
         }
         if(isset($_SESSION['type']) && isset($_SESSION['username'])){
-            header('location: navbar.php');
+            header('location: ../index.php');
             $cnnx = null;
         }
     }
