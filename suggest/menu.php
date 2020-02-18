08dcaaea3d06c5f7436918e54e19bd2b025e4a9a
Phil KREJCI <kphil@protonmail.com>
2020-02-19 00:10:39 +0100

    suggest/

---
+++ suggest/menu.php
@@ -0,0 +1,24 @@
+<html>
+  <link rel="stylesheet" href="./suggest/menu.css">
+  <body>
+    <div id="suggest">
+<?php
+$cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','yellowtree','yellow');
+$sql = $cnnx -> prepare("SELECT title, posterurl FROM `MOVIE` ");
+$sql -> execute();
+$films = $sql -> fetchAll();
+foreach($films as $film){
+?>
+
+  <div class="sep-sugg">
+    <div class="suggfilm">
+      <img src="<?php print_r($film['posterurl']); ?>" ></img>
+      <div class="label"><?php print_r($film['title']); ?></div>
+    </div>
+  </div>
+
+
+<?php  } ?>
+    </div>
+  <body>
+</html>
