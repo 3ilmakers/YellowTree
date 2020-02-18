135d6478015b3c96f12b3b5de3e2801cf9b70ff1
Jeremy <37037667+jaydainn@users.noreply.github.com>
2020-02-18 23:10:33 +0100

    modify

--- dashboard/admin.php
+++ dashboard/admin.php
@@ -1,159 +1,169 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="./dashboard/dashboard.css">
-    <script src="https://cdn.anychart.com/js/8.0.1/anychart-core.min.js"></script>
-    <script src="https://cdn.anychart.com/js/8.0.1/anychart-pie.min.js"></script>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>YellowTree</title>
-  </head>
-  <!--<body >-->
-    <?php
-    
-    if(isset($_SESSION['type']) && $_SESSION['type'] == "admin"){}
-    else{
-      header('location: ../index.php');
-    }
-    ?>
-    <div class="container-fluid">
-    <div  class="row">
-      <div id="sidebar" class="col-3 rounded">
-        <ul>
-          <li class="rounded">
-            <h4>Welcome Back</h4>
-          </li>
-          <div class="hr"></div>
-          <li class="rounded hover">
-            <a><i class="	fas fa-desktop"></i><span> Users</span></a>
-          </li>
-          <div class="hr"></div>
-          <li class="rounded hover">
-            <a><i class="	fas fa-video"></i><span> Movies</span></a>
-          </li>
-        </ul>
-      </div>
-      <div class="col-9 rounded" id="mainpanel">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="./dashboard/dashboard.css">
+  <script src="https://cdn.anychart.com/js/8.0.1/anychart-core.min.js"></script>
+  <script src="https://cdn.anychart.com/js/8.0.1/anychart-pie.min.js"></script>
 
-        <h4 id="maintext">Change users type</h4>
+  <title>YellowTree</title>
+</head>
+<!--<body >-->
+<?php
 
-        <form method="GET" action="<?php echo $_SERVER["PHP_SELF"];?>" class="text-center rounded">
-          <input type="text" id="input" class="rounded" name="username" placeholder="Enter Username">
-           <input type="hidden" name="dashboard" value="admin" />
-          <input type="submit" id="btn" value="Search" class="rounded">
-        </form>
-        <div class="yellowhr"></div>
-        <div id="userslider" class="container rounded">
+if (isset($_SESSION['type']) && $_SESSION['type'] == "admin") {
+} else {
+  header('location: ../index.php');
+}
+?>
+<div class="container-fluid">
+  <div class="row">
+  <div style="position: relative;" class="col-3 rounded"></div>
+  <div id="sidebar" class="col-3 rounded">
+      <ul>
+        <li class="rounded">
+          <h4>Welcome Back</h4>
+        </li>
+        <div class="hr"></div>
+        <li class="rounded hover">
+          <a><i class="	fas fa-desktop"></i><span> Users</span></a>
+        </li>
+        <div class="hr"></div>
+        <li class="rounded hover">
+          <a><i class="	fas fa-video"></i><span> Movies</span></a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-9 rounded" id="mainpanel">
 
-          <div class="text-center">
-            <div class="row">
-           <?php
-     class user{
-       public $uname ;
-       public $fname ;
-       public $lname ;
-       public $email ;
-       public $type ;
-       public $pword;
+      <h4 id="maintext">Change users type</h4>
 
-        function __construct( $username , $firstname , $lastname, $email, $type, $pword){
-          $this->uname = $username;
-          $this->fname = $firstname;
-          $this->lname = $lastname;
-          $this->email = $email;
-          $this->type = $type;
-          $this->pword = $pword;
-        }
+      <form method="GET" action="<?php echo $_SERVER["PHP_SELF"]; ?>" class="text-center rounded">
+        <input type="text" id="input" class="rounded" name="username" placeholder="Enter Username">
+        <input type="hidden" name="dashboard" value="admin" />
+        <input type="submit" id="btn" value="Search" class="rounded">
+      </form>
+      <div class="yellowhr"></div>
+      <div id="userslider" class="container rounded overflow-auto">
 
-        function display() {
-          echo "<div class=\"col-4\">
+        <div class="text-center">
+          <div class="row">
+
+            <?php
+            class user
+            {
+              public $uname;
+              public $fname;
+              public $lname;
+              public $email;
+              public $type;
+              public $pword;
+
+              function __construct($username, $firstname, $lastname, $email, $type, $pword)
+              {
+                $this->uname = $username;
+                $this->fname = $firstname;
+                $this->lname = $lastname;
+                $this->email = $email;
+                $this->type = $type;
+                $this->pword = $pword;
+              }
+
+              function display()
+              {
+                echo "<div class=\"col-4\">
           <div id=\"containeruser\" data-toggle=\"modal\" data-target=\"#modifyusertype\" class=\"text-center rounded\">
             <div class=\"hr\"></div>
             <i  class=\"fas fa-user userimg\"></i>
             <div class=\"hr\"></div>
-            <p  class=\"userdesc username\">Username :".$this->uname."</p>
+            <p  class=\"userdesc username\">Username :" . $this->uname . "</p>
             <div class=\"hr\"></div>
-            <p class=\"userdesc\">First Name :".$this->fname."</p>
+            <p class=\"userdesc\">First Name :" . $this->fname . "</p>
             <div class=\"hr\"></div>
-            <p class=\"userdesc\">Last Name :".$this->lname."</p>
+            <p class=\"userdesc\">Last Name :" . $this->lname . "</p>
             <div class=\"hr\"></div>
-            <p class=\"userdesc\">Email :".$this->email." </p>
+            <p class=\"userdesc\">Email :" . $this->email . " </p>
             <div class=\"hr\"></div>
-            <p class=\"userdesc\">Password :".$this->pword."</p>
+            <p class=\"userdesc\">Password :" . $this->pword . "</p>
             <div class=\"hr\"></div>
-            <p class=\"userdesc\">Type :".$this->type."</p>
+            <p class=\"userdesc\">Type :" . $this->type . "</p>
             <div class=\"hr\"></div>
           </div>
         </div>";
-        }
-     }
+              }
+            }
 
-if($_SESSION['type'] == "admin"){
-    $username = $_GET['username'];
-    $username = "%".$username."%" ;
-    //$usersarray[] = new user("","","","","","");
-    //echo $_GET['username'];
-    $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','yellowtree','yellow');
-    $sql = $cnnx -> prepare("SELECT * FROM `USERS` WHERE username LIKE :username ");
-    $sql -> execute([':username' =>  $username]);
-    $users = $sql -> fetchAll();
+            if ($_SESSION['type'] == "admin") {
+              $username = $_GET['username'];
+              $username = "%" . $username . "%";
+              $count = 0;
+              //$usersarray[] = new user("","","","","","");
+              //echo $_GET['username'];
+              $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost', 'yellowtree', 'yellow');
+              $sql = $cnnx->prepare("SELECT * FROM `USERS` WHERE username LIKE :username ");
+              $sql->execute([':username' =>  $username]);
+              $users = $sql->fetchAll();
 
-    foreach($users as $user){
-      $usersarray[] = new user($user['username'] , $user['firstname'], $user['lastname'], $user['email'], $user['type'],$user['password']);
+              foreach ($users as $user) {
+                $usersarray[] = new user($user['username'], $user['firstname'], $user['lastname'], $user['email'], $user['type'], $user['password']);
+              }
 
-    }
+              if (sizeof($usersarray) > 3) {
+                foreach ($usersarray as $usertodisplay) {
+                  $count++;
+                  $usertodisplay->display();
+                  if ($count % 3 == 0) {
+                    echo "<div class=\"w-100\"></div>";
+                  }
+                }
+              } else {
+                foreach ($usersarray as $usertodisplay) {
+                  $usertodisplay->display();
+                }
+              }
+            }
+            ?>
 
 
-    foreach($usersarray as $usertodisplay){
-      $usertodisplay -> display();
-    }
-    $cnnx = null ;
 
-}
-else {
-    echo "get lost son of a bitch";
-}
-    ?>
-
-
-
-
-            </div>
 
           </div>
 
         </div>
+
+
         <div class="yellowhr"></div>
         <?php
-          $nbadmin = 0;
-          $nbuser = 0;
-          $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','yellowtree','yellow');
-          $sql = $cnnx -> prepare("SELECT COUNT(username) as nbadmin FROM USERS WHERE type = \"admin\";");
-          $sql -> execute();
-          $admins = $sql -> fetchAll();
-          foreach($admins as $admin){
-            $nbadmin =$nbadmin + $admin['nbadmin'];
-          }
-          $sql = null;
-          $sql = $cnnx -> prepare("SELECT COUNT(username) as nbuser FROM USERS WHERE type = \"user\";");
-          $sql -> execute();
-          $users = $sql -> fetchAll();
-          //print_r($users);
-          foreach($users as $user){
-            $nbuser = $nbuser + $user['nbuser'];
-          }
+        $nbadmin = 0;
+        $nbuser = 0;
+        $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost', 'yellowtree', 'yellow');
+        $sql = $cnnx->prepare("SELECT COUNT(username) as nbadmin FROM USERS WHERE type = \"admin\";");
+        $sql->execute();
+        $admins = $sql->fetchAll();
+        foreach ($admins as $admin) {
+          $nbadmin = $nbadmin + $admin['nbadmin'];
+        }
+        $sql = null;
+        $sql = $cnnx->prepare("SELECT COUNT(username) as nbuser FROM USERS WHERE type = \"user\";");
+        $sql->execute();
+        $users = $sql->fetchAll();
+        //print_r($users);
+        foreach ($users as $user) {
+          $nbuser = $nbuser + $user['nbuser'];
+        }
 
-          echo "<script> anychart.onDocumentReady(function() {
+        echo "<script> anychart.onDocumentReady(function() {
 
             // set the data
             var data = [
-                {x: \"Admin\", value:".$nbadmin."},
-                {x: \"User\", value:".$nbuser."}
+                {x: \"Admin\", value:" . $nbadmin . "},
+                {x: \"User\", value:" . $nbuser . "}
 
             ];
 
@@ -195,27 +205,24 @@ else {
         <p id="targetusername"> Username </p>
         <div class="hr"></div>
 
-
-        <div class="custom-control custom-switch">
-
-          <input type="checkbox" class="custom-control-input" id="customSwitch1">
-
-        </div>
         <br>
         <div class="yellowhr"></div>
       </div>
-      </div>
     </div>
-
   </div>
+</div>
 
-    <!-- Optional JavaScript -->
 
-    <script src="dashboard.js"></script>
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://kit.fontawesome.com/4dded3e0b7.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  <!--</body>-->
+
+
+<!-- Optional JavaScript -->
+
+<script src="dashboard.js"></script>
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://kit.fontawesome.com/4dded3e0b7.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<!--</body>-->
+
 </html>
--- navbar/signup.php
+++ navbar/signup.php
@@ -5,7 +5,7 @@ $lastname =$_POST['lastname'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $type ="user";
-$cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','root','root');
+$cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','yellowtree','yellow');
 $sql = $cnnx -> prepare( "INSERT INTO `USERS` (`username`, `email`, `firstname`, `lastname`, `password`, `type`) VALUES (:username, :email, :firstname, :lastname, :password, :type);");
 $sql -> execute([':username' => $username,':email' => $email, ':firstname' => $firstname, ':lastname' => $lastname, ':password' => $password, 'type' => $type]);
 header('location: ../index.php');
