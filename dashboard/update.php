61a7fd7817bf321651350495bf266e0379985b6f
Jeremy dain <37037667+jaydainn@users.noreply.github.com>
2020-02-18 16:27:30 +0100

    updated

--- dashboard/update.php
+++ dashboard/update.php
@@ -7,5 +7,6 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','yellowtree','yellow');
 $sql = $cnnx -> prepare("UPDATE `users` SET `email`= :email,`firstname`= :firstname,`lastname`= :lastname ,`password`= :password WHERE username = :username ");
-$sql -> execute([':email' => $email , ':firstname' => $firstname , ':lastname' => $lastname, ':password' => $password]);
-header('location : user.php');
+$sql -> execute([':email' => $email , ':firstname' => $firstname , ':lastname' => $lastname, ':password' => $password, ':username' => $_SESSION['username']]);
+$cnnx = null;
+header('location: user.php');
--- navbar/signup.php
+++ navbar/signup.php
@@ -7,7 +7,7 @@ $password = $_POST['password'];
 $type ="user";
 $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','yellowtree','yellow');
 $sql = $cnnx -> prepare( "INSERT INTO `users` (`username`, `email`, `firstname`, `lastname`, `password`, `type`) VALUES (:username, :email, :firstname, :lastname, :password, :type);");
-$sql -> execute(['username' => $username,'email' => $email, 'firstname' => $firstname, 'lastname' => $lastname, 'password' => $password, 'type' => $type]);
+$sql -> execute([':username' => $username,':email' => $email, ':firstname' => $firstname, ':lastname' => $lastname, ':password' => $password, 'type' => $type]);
 header('location: navbar.php');
 $cnnx = null ;
 
