333e2453bcfc01f298a2f3b98d94d9752cd6ebfe
Phil KREJCI <kphil@protonmail.com>
2020-02-18 16:50:22 +0100

    redirect to index.php

--- navbar/logout.php
+++ navbar/logout.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
  session_start();
  if(isset($_SESSION['username']) && isset($_SESSION['type'])){
      session_destroy();
-     header('location: navbar.php');
-     
-    
+     header('location: ../index.php');
+
+
 
  }
  else {
-    header('location: navbar.php');
+    header('location: ../index.php');
  }
--- navbar/signup.php
+++ navbar/signup.php
@@ -1,5 +1,5 @@
 <?php
-$username = $_POST['username'];
+/*$username = $_POST['username'];
 $firstname =$_POST['firstname'];
 $lastname =$_POST['lastname'];
 $email = $_POST['email'];
@@ -7,9 +7,9 @@ $password = $_POST['password'];
 $type ="user";
 $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','yellowtree','yellow');
 $sql = $cnnx -> prepare( "INSERT INTO `users` (`username`, `email`, `firstname`, `lastname`, `password`, `type`) VALUES (:username, :email, :firstname, :lastname, :password, :type);");
-$sql -> execute([':username' => $username,':email' => $email, ':firstname' => $firstname, ':lastname' => $lastname, ':password' => $password, 'type' => $type]);
-header('location: index.php');
-$cnnx = null ;
+$sql -> execute([':username' => $username,':email' => $email, ':firstname' => $firstname, ':lastname' => $lastname, ':password' => $password, 'type' => $type]);*/
+header('location: ../index.php');
+//$cnnx = null ;
 
 
 
