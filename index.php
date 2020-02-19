19334351427ecd4a38a62daed73796c7f6869108
Phil KREJCI <kphil@protonmail.com>
2020-02-19 01:20:59 +0100

    SECURE redirect

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-
-
+  session_start();
+ini_set('display_errors', 'On');
 
   require_once("./navbar/navbar.php");
 ?>
@@ -9,12 +9,27 @@
 if(isset($_GET['dashboard']))
 {
   $dashboard = $_GET['dashboard'];
-  if( $dashboard == "admin")    require_once("./dashboard/admin.php");
+  if( $dashboard == "admin") {
+
+      if(isset($_SESSION['type'] ))
+      {
+          if ($_SESSION['type'] != "admin"){
+            include_once("./suggest/menu.php");
+          }
+          else {
+            require_once("./dashboard/admin.php");
+          }
+      } else {
+          include_once("./suggest/menu.php");
+      }
+
+  }
   if( $dashboard == "user")    require_once("./dashboard/user.php");
 }
 else {
   include_once("./suggest/menu.php");
 }
+
 
 
 
