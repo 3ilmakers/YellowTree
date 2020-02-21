acb04a3f3cbd5483d722b4dfa735824ad6ef1fc1
Phil KREJCI <kphil@protonmail.com>
2020-02-21 15:14:58 +0100

    md5 password

--- navbar/login.php
+++ navbar/login.php
@@ -9,7 +9,7 @@ else{
     $password = $_POST['password'];
     $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost','yellowtree','yellow');
     $sql = $cnnx -> prepare("SELECT type , username FROM `USERS` WHERE email = :email AND password = :password ");
-    $sql -> execute([':email' => $email, ':password' => $password]);
+    $sql -> execute([':email' => $email, ':password' => md5($password)]);
     $type = $sql -> fetchAll();
     echo "TYPES : " . $types;
     if($types=="") header('location: ../index.php?login_status=wrong');
