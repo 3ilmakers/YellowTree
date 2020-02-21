97f1af2c05f7335647e2ef1fbabc6fd620a00b9e
Jeremy dain <37037667+jaydainn@users.noreply.github.com>
2020-02-21 22:12:17 +0100

    Update changetype.php

--- dashboard/changetype.php
+++ dashboard/changetype.php
@@ -12,9 +12,9 @@ else{
   $changedtype ="admin";
 }
 $cnnx = new PDO('mysql:dbname=yellowtree;host=localhost', 'yellowtree', 'yellow');
-$sql = $cnnx->prepare("UPDATE users SET type = :type WHERE username= :username");
+$sql = $cnnx->prepare("UPDATE users SET type = :type WHERE username = :username");
 $sql -> execute([':type' => $changedtype ,':username' => $username]);
-
+$cnnx = null;
 echo $username." was updated to ".$changedtype;
 
 
