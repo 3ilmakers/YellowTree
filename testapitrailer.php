02f3687b2be09e22df124976a5700bc43b072efb
Jeremy dain <37037667+jaydainn@users.noreply.github.com>
2020-02-22 16:39:04 +0100

    api test

--- testapitrailer.php
+++ testapitrailer.php
@@ -4,8 +4,8 @@
         return str_replace(" ", "-", $title);
     }
 
-    echo formatTitle("OnCe Upon A Time In Hollywood");
+    echo formatTitle("u");
 
-$upcoming = file_get_contents("http://api.traileraddict.com/?film=".formatTitle("OnCe Upon A Time In Hollywood"));
+$upcoming = file_get_contents("https://api.themoviedb.org/3/search/movie?api_key=7b457abe8149f666a244bf5b9f51d79b&query=".formatTitle("forrest gump"));
 print_r($upcoming);
 
